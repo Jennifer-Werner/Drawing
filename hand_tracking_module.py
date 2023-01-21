83012ce86f0a560a091a290a7c2ed3d22af4f545
Tina <trantina375657@gmail.com>
2023-01-21 14:31:05 +0300

    Delete unneeded

--- hand_tracking_module.py
+++ hand_tracking_module.py
@@ -127,36 +127,3 @@ class handDetector():
                 self.x, self.y = self.landmark_list[8][1], self.landmark_list[8][2]
 
         return img
-
-def main():
-
-    prevT = 0
-    conT = 0
-    cap = cv2.VideoCapture(0)
-    detector = handDetector()
-
-    while True:
-
-        ret, frame = cap.read()
-        if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
-            break
-        flipped = np.fliplr(frame)
-        flipped_RGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
-
-        flipped_RGB = detector.findHands(flipped_RGB)
-        landmark_list = detector.trackHands(flipped_RGB)
-        if len(landmark_list) != 0:
-            print(landmark_list[8])
-
-        conT = time.time()
-        fps = 1 / (conT - prevT)
-        prevT = conT
-
-        cv2.putText(flipped_RGB, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
-        res_image = cv2.cvtColor(flipped_RGB, cv2.COLOR_RGB2BGR)
-        cv2.imshow('Image', res_image)
-
-
-if __name__ == '__main__':
-    main()
